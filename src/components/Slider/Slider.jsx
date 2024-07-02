6b6eb096bbb71b6f31f915d0b0764457338c8e7e
minhaj-boop <imminhajj@gmail.com>
2024-07-03 01:33:21 +0600

    first commit

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import EastIcon from "@mui/icons-material/East";
+import WestIcon from "@mui/icons-material/West";
+import "./Slider.scss";
+
+const data = [
+  "https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+];
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+  };
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+  };
+
+  return (
+    <div className="slider">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon" onClick={prevSlide}>
+          <WestIcon />
+        </div>
+        <div className="icon" onClick={nextSlide}>
+          <EastIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
